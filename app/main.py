9447c336d8e4e2ea48bf758d0a26fff9138613ea
param-vora <paramvora999@gmail.com>
2025-03-03 13:52:43 -0500

    Added buildings and operators router.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from app.endpoints import leads, rooms, tenants
+from app.endpoints import leads, rooms, tenants, buildings, operators
 
 app = FastAPI()
 
 app.include_router(leads.router)
 app.include_router(rooms.router)
 app.include_router(tenants.router)
-
+app.include_router(buildings.router)
+app.include_router(operators.router)
 
 @app.get("/")
 async def root():
